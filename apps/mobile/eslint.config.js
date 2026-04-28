ad95e8ec2ad46ba23684b14e2da8e7e1c7c822e2
SoJDev | Hoàng Lâm <lhlam2515@gmail.com>
2026-04-28 23:28:54 +0700

    feat: add Prettier and ESLint configuration for code formatting and linting

--- apps/mobile/eslint.config.js
+++ apps/mobile/eslint.config.js
@@ -1,10 +1,120 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+const prettier = require("eslint-config-prettier");
+const boundaries = require("eslint-plugin-boundaries");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 
 module.exports = defineConfig([
   expoConfig,
+  prettier,
+  eslintPluginPrettierRecommended,
   {
-    ignores: ['dist/*'],
+    rules: {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+            "object",
+            "type",
+          ],
+          "newlines-between": "always",
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+    ignores: ["components/ui/**"],
+  },
+  {
+    plugins: { boundaries },
+    settings: {
+      "boundaries/include": ["src/**/*"],
+      "boundaries/elements": [
+        {
+          mode: "full",
+          type: "shared",
+          pattern: [
+            "src/components/**/*",
+            "src/context/**/*",
+            "src/constants/**/*",
+            "src/hooks/**/*",
+            "src/lib/**/*",
+            "src/types/**/*",
+            "src/assets/**/*",
+          ],
+        },
+        {
+          mode: "full",
+          type: "feature",
+          capture: ["featureName"],
+          pattern: ["src/features/*/**/*"],
+        },
+        {
+          mode: "full",
+          type: "widget",
+          pattern: ["src/widgets/**/*"],
+        },
+        {
+          mode: "full",
+          type: "app",
+          capture: ["_", "fileName"],
+          pattern: ["src/app/**/*"],
+        },
+        {
+          mode: "full",
+          type: "neverImport",
+          pattern: ["src/*"],
+        },
+      ],
+    },
+    rules: {
+      "boundaries/no-unknown": ["error"],
+      "boundaries/no-unknown-files": ["error"],
+      "boundaries/element-types": [
+        "error",
+        {
+          default: "disallow",
+          rules: [
+            {
+              from: ["shared"],
+              allow: ["shared"],
+            },
+            {
+              from: ["feature"],
+              allow: [
+                "shared",
+                ["feature", { featureName: "${from.featureName}" }],
+              ],
+            },
+            {
+              from: ["widget"],
+              allow: ["shared", "feature"],
+            },
+            {
+              from: ["app", "neverImport"],
+              allow: ["shared", "feature", "widget"],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    ignores: [".expo/**", "dist/**", "node_modules/**"],
   },
 ]);
